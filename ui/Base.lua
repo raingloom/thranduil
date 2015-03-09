80eeda9e087887d2f47c8cee69290255d924c4e3
adn <adonaac@gmail.com>
2015-03-08 23:52:21 -0300

    addp Added Base mixin

--- ui/Base.lua
+++ ui/Base.lua
@@ -1,11 +1,8 @@
-local Object = require((...):sub(1, (...):find('/')) .. 'classic/classic')
-local Checkbox = Object:extend('Checkbox')
+local ui_path = (...):sub(1, (...):find('/'))
+local Object = require(ui_path .. 'classic/classic')
+local Base = Object:extend('Base')
 
-function Checkbox:new(ui, x, y, w, h, settings)
-    self.ui = ui
-    self.id = self.ui.addToElementsList(self)
-    self.type = 'Checkbox'
-
+function Base:baseNew(x, y, w, h, settings)
     self.ix, self.iy = x, y
     self.x, self.y = x, y
     self.w, self.h = w, h
@@ -13,35 +10,32 @@ function Checkbox:new(ui, x, y, w, h, settings)
     for k, v in pairs(settings) do self[k] = v end
 
     self.input = self.ui.Input()
-    self.input:bind(' ', 'check-toggle')
-    self.input:bind('mouse1', 'left-click')
+    self:bind('mouse1', 'left-click')
+    self:bind('return', 'key-enter')
 
     self.hot = false
     self.selected = false
-    self.down = false 
+    self.down = false
     self.pressed = false
     self.released = false
     self.enter = false
     self.exit = false
     self.selected_enter = false
     self.selected_exit = false
-    self.checked_enter = false
-    self.checked_exit = false
-
-    self.pressing = false
-    self.previous_hot = false
-    self.previous_selected = false
-    self.previous_checked = false
-
-    self.checked = false
 
     -- Initialize extensions
     for _, extension in ipairs(self.extensions or {}) do
         if extension.new then extension.new(self) end
     end
+
+    self.pressing = false
+    self.previous_hot = false
+    self.previous_selected = false
+    self.previous_pressed = false
+    self.previous_released = false
 end
 
-function Checkbox:update(dt, parent)
+function Base:basePreUpdate(dt, parent)
     local x, y = love.mouse.getPosition()
     if parent then self.x, self.y = parent.x + self.ix, parent.y + self.iy end
 
@@ -60,6 +54,13 @@ function Checkbox:update(dt, parent)
         self.exit = true
     else self.exit = false end
 
+    -- Set focused or not
+    if self.hot and self.input:pressed('left-click') then
+        self.selected = true
+    elseif not self.hot and self.input:pressed('left-click') then
+        self.selected = false
+    end
+
     -- Check for selected_enter
     if self.selected and not self.previous_selected then
         self.selected_enter = true
@@ -69,16 +70,6 @@ function Checkbox:update(dt, parent)
     if not self.selected and self.previous_selected then
         self.selected_exit = true
     else self.selected_exit = false end
-
-    -- Check for checked_enter
-    if self.checked and not self.previous_checked then
-        self.checked_enter = true
-    else self.checked_enter = false end
-
-    -- Check for checked_exit
-    if not self.checked and self.previous_checked then
-        self.checked_exit = true
-    else self.checked_exit = false end
 
     -- Check for pressed/released/down on mouse hover
     if self.hot and self.input:pressed('left-click') then
@@ -95,24 +86,21 @@ function Checkbox:update(dt, parent)
     end
 
     -- Check for pressed/released/down on key press
-    if self.selected and self.input:pressed('check-toggle') then
+    if self.selected and self.input:pressed('key-enter') then
         self.pressed = true
         self.pressing = true
     end
-    if self.pressing and self.input:down('check-toggle') then
+    if self.pressing and self.input:down('key-enter') then
         self.down = true
     end
-    if self.pressing and self.input:released('check-toggle') then
+    if self.pressing and self.input:released('key-enter') then
         self.released = true
         self.pressing = false
         self.down = false
     end
+end
 
-    -- Change state
-    if self.released and (self.hot or self.selected) then
-        self.checked = not self.checked
-    end
-
+function Base:basePostUpdate(dt)
     if self.pressed and self.previous_pressed then self.pressed = false end
     if self.released and self.previous_released then self.released = false end
 
@@ -120,22 +108,24 @@ function Checkbox:update(dt, parent)
     for _, extension in ipairs(self.extensions or {}) do
         if extension.update then extension.update(self, dt, parent) end
     end
-
-    -- Set previous frame state
+    
     self.previous_hot = self.hot
     self.previous_pressed = self.pressed
     self.previous_released = self.released
     self.previous_selected = self.selected
-    self.previous_checked = self.checked
 
     self.input:update(dt)
 end
 
-function Checkbox:draw()
+function Base:baseDraw()
     -- Draw extensions
     for _, extension in ipairs(self.extensions or {}) do
         if extension.draw then extension.draw(self) end
     end
 end
 
-return Checkbox
+function Base:bind(key, action)
+    self.input:bind(key, action)
+end
+
+return Base
--- ui/Button.lua
+++ ui/Button.lua
@@ -1,132 +1,30 @@
-local Object = require((...):sub(1, (...):find('/')) .. 'classic/classic')
+local ui_path = (...):sub(1, (...):find('/'))
+local Object = require(ui_path .. 'classic/classic')
+local Base = require(ui_path .. 'Base')
 local Button = Object:extend('Button')
+Button:implement(Base)
 
 function Button:new(ui, x, y, w, h, settings)
     self.ui = ui
     self.id = self.ui.addToElementsList(self)
     self.type = 'Button'
 
-    self.ix, self.iy = x, y
-    self.x, self.y = x, y
-    self.w, self.h = w, h
-    local settings = settings or {}
-    for k, v in pairs(settings) do self[k] = v end
-
-    self.input = self.ui.Input()
-    self.input:bind('return', 'key-enter')
-    self.input:bind('mouse1', 'left-click')
-
-    self.hot = false
-    self.selected = false
-    self.down = false
-    self.pressed = false
-    self.released = false
-    self.enter = false
-    self.exit = false
-    self.selected_enter = false
-    self.selected_exit = false
-
-    self.pressing = false
-    self.previous_hot = false
-    self.previous_selected = false
-    self.previous_pressed = false
-    self.previous_released = false
-
-    -- Initialize extensions
-    for _, extension in ipairs(self.extensions or {}) do
-        if extension.new then extension.new(self) end
-    end
+    self:baseNew(x, y, w, h, settings)
 end
 
 function Button:update(dt, parent)
-    local x, y = love.mouse.getPosition()
     if parent then 
         if parent.type == 'Frame' and self.annotation == "Frame's close button" then
             self.ix = parent.w - parent.close_margin - parent.close_button_width
             self.iy = parent.close_margin
         end
-        self.x, self.y = parent.x + self.ix, parent.y + self.iy 
     end
-
-    -- Check for hot
-    if x >= self.x and x <= self.x + self.w and y >= self.y and y <= self.y + self.h then
-        self.hot = true
-    else self.hot = false end
-
-    -- Check for enter 
-    if self.hot and not self.previous_hot then
-        self.enter = true
-    else self.enter = false end
-
-    -- Check for exit
-    if not self.hot and self.previous_hot then
-        self.exit = true
-    else self.exit = false end
-
-    -- Check for selected_enter
-    if self.selected and not self.previous_selected then
-        self.selected_enter = true
-    else self.selected_enter = false end
-
-    -- Check for selected_exit
-    if not self.selected and self.previous_selected then
-        self.selected_exit = true
-    else self.selected_exit = false end
-
-    -- Check for pressed/released/down on mouse hover
-    if self.hot and self.input:pressed('left-click') then
-        self.pressed = true
-        self.pressing = true
-    end
-    if self.pressing and self.input:down('left-click') then
-        self.down = true
-    end
-    if self.pressing and self.input:released('left-click') then
-        self.released = true
-        self.pressing = false
-        self.down = false
-    end
-
-    -- Check for pressed/released/down on key press
-    if self.selected and self.input:pressed('key-enter') then
-        self.pressed = true
-        self.pressing = true
-    end
-    if self.pressing and self.input:down('key-enter') then
-        self.down = true
-    end
-    if self.pressing and self.input:released('key-enter') then
-        self.released = true
-        self.pressing = false
-        self.down = false
-    end
-
-    -- Update extensions
-    for _, extension in ipairs(self.extensions or {}) do
-        if extension.update then extension.update(self, dt, parent) end
-    end
-
-    if self.pressed and self.previous_pressed then self.pressed = false end
-    if self.released and self.previous_released then self.released = false end
-
-    -- Set previous frame state
-    self.previous_hot = self.hot
-    self.previous_pressed = self.pressed
-    self.previous_released = self.released
-    self.previous_selected = self.selected
-
-    self.input:update(dt)
+    self:basePreUpdate(dt, parent)
+    self:basePostUpdate(dt)
 end
 
 function Button:draw()
-    -- Draw extensions
-    for _, extension in ipairs(self.extensions or {}) do
-        if extension.draw then extension.draw(self) end
-    end
-end
-
-function Button:bind(key, action)
-    self.input:bind(key, action)
+    self:baseDraw()
 end
 
 function Button:press()
