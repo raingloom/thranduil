4f498687ecd7a72385fed9d566063c18459cc668
adn <adonaac@gmail.com>
2015-05-21 01:14:34 -0300

    Added left/right/up/down selection for a container, defaults to arrow keys with actions named focus-left, focus-right, focus-up, focus-down

--- ui/Container.lua
+++ ui/Container.lua
@@ -8,6 +8,10 @@ function Container:containerNew(settings)
     self:bind('tab', 'focus-next')
     self:bind('lshift', 'previous-modifier')
     self:bind('escape', 'unselect')
+    self:bind('left', 'focus-left')
+    self:bind('right', 'focus-right')
+    self:bind('up', 'focus-up')
+    self:bind('down', 'focus-down')
 
     self.elements = {}
     self.currently_focused_element = nil
@@ -18,6 +22,10 @@ function Container:containerUpdate(dt, parent)
     -- Focus on elements
     if self.selected and not self.input:down('previous-modifier') and self.input:pressed('focus-next') then self:focusNext() end
     if self.selected and self.input:down('previous-modifier') and self.input:pressed('focus-next') then self:focusPrevious() end
+    if self.selected and self.input:pressed('focus-left') then self:focusDirection('left') end
+    if self.selected and self.input:pressed('focus-right') then self:focusDirection('right') end
+    if self.selected and self.input:pressed('focus-up') then self:focusDirection('up') end
+    if self.selected and self.input:pressed('focus-down') then self:focusDirection('down') end
     for i, element in ipairs(self.elements) do
         if not element.selected or not i == self.currently_focused_element then
             element.selected = false
@@ -96,6 +104,63 @@ function Container:focusPrevious()
     else self.currently_focused_element = #self.elements end
 end
 
+function Container:focusDirection(direction)
+    for _, element in ipairs(self.elements) do if element.any_selected then return end end
+    for i, element in ipairs(self.elements) do 
+        if element.selected then self.currently_focused_element = i end
+        element.selected = false 
+    end
+    if self.currently_focused_element then
+        local angled = function(a, b) local d = math.abs(a - b) % 360; if d > 180 then return 360 - d else return d end end
+        local angles = {right = 0, left = -180, up = -90, down = 90}
+        local min, j, n = 10000, nil, 0
+        local selected_element = self.elements[self.currently_focused_element]
+        for i, element in ipairs(self.elements) do
+            if i ~= self.currently_focused_element then
+                local d = math.sqrt(math.pow(selected_element.x - element.x, 2) + math.pow(selected_element.y - element.y, 2))
+                local v = math.max(angled(math.deg(math.atan2(element.y - selected_element.y, element.x - selected_element.x)), angles[direction]), 20)*d
+                if direction == 'right' then if element.x > selected_element.x then n = n + 1 end end
+                if direction == 'left' then if element.x < selected_element.x then n = n + 1 end end
+                if direction == 'up' then if element.y < selected_element.y then n = n + 1 end end
+                if direction == 'down' then if element.y > selected_element.y then n = n + 1 end end
+                if v <= min then min = v; j = i end
+            end
+        end
+        -- Loop over
+        if n == 0 then
+            if direction == 'left' or direction == 'right' then
+                max_dx, min_dy, j = -10000, 10000, nil
+                for i, element in ipairs(self.elements) do
+                    if i ~= self.currently_focused_element then
+                        local dx, dy = math.abs(selected_element.x - element.x), math.abs(selected_element.y - element.y)
+                        if dx > max_dx or dy < min_dy then
+                            max_dx = dx
+                            min_dy = dy
+                            j = i
+                        end
+                    end
+                end
+                if j then self.currently_focused_element = j end
+            elseif direction == 'up' or direction == 'down' then
+                min_dx, max_dy, j = 10000, -10000, nil
+                for i, element in ipairs(self.elements) do
+                    if i ~= self.currently_focused_element then
+                        local dx, dy = math.abs(selected_element.x - element.x), math.abs(selected_element.y - element.y)
+                        if dx < min_dx or dy > max_dy then
+                            min_dx = dx
+                            max_dy = dy
+                            j = i
+                        end
+                    end
+                end
+                if j then self.currently_focused_element = j end
+            end
+
+        -- Normal behavior
+        elseif n > 0 and j then self.currently_focused_element = j end
+    else self.currently_focused_element = 1 end
+end
+
 function Container:unselect()
     for _, element in ipairs(self.elements) do if element.any_selected then return end end
     if self.any_selected then
@@ -103,9 +168,7 @@ function Container:unselect()
             element.selected = false
             self.currently_focused_element = nil
         end
-    else
-        self.selected = false
-    end
+    else self.selected = false end
 end
 
 return Container
